75d8cb5636106472e7e8ff6b9b8f8ae7faba74e5
Martin Keegan <martin@no.ucant.org>
2019-10-23 17:10:01 +0100

    add doxology / indent

--- main.py
+++ main.py
@@ -10,43 +10,46 @@ from pdf import Pdf
 from metadata import Metadata
 from checks import path_checks, file_checks, dependencies_checks
 
+def run():
+    parser = argparse.ArgumentParser(description='chapter-splitter')
 
-parser = argparse.ArgumentParser(description='chapter-splitter')
+    parser.add_argument('input_file',
+                        help = 'PDF file to elaborate')
+    parser.add_argument('output_folder',
+                        help = 'Output folder where to store the new PDFs')
+    parser.add_argument('-d', '--doi',
+                        help = 'The DOI (at book-level) you wish to parse',
+                        required = True)
 
-parser.add_argument('input_file',
-		    help = 'PDF file to elaborate')
-parser.add_argument('output_folder',
-		    help = 'Output folder where to store the new PDFs')
-parser.add_argument('-d', '--doi',
-		    help = 'The DOI (at book-level) you wish to parse',
-		    required = True)
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    # Check parsed arguments
+    file_checks(args.input_file)
+    path_checks(args.output_folder)
 
-# Check parsed arguments
-file_checks(args.input_file)
-path_checks(args.output_folder)
+    # Check dependencies
+    dependencies_checks()
 
-# Check dependencies
-dependencies_checks()
+    # Discover chapter-level DOIs of the supplied --doi value
+    d = Doi(args.doi)
+    ch_dois = d.discover_ch_dois()
 
-# Discover chapter-level DOIs of the supplied --doi value
-d = Doi(args.doi)
-ch_dois = d.discover_ch_dois()
+    m = Metadata()
+    p = Pdf(args.input_file, args.output_folder)
 
-m = Metadata()
-p = Pdf(args.input_file, args.output_folder)
+    for doi in ch_dois:
+        # Gather metadata for 'doi'
+        doi_metadata = m.gather_metadata(doi)
 
-for doi in ch_dois:
-    # Gather metadata for 'doi'
-    doi_metadata = m.gather_metadata(doi)
+        # Produce the PDF
+        page_range = p.get_page_range(doi_metadata['page_range'])
+        output_file_name = doi.split('/')[1] + '.pdf'
 
-    # Produce the PDF
-    page_range = p.get_page_range(doi_metadata['page_range'])
-    output_file_name = doi.split('/')[1] + '.pdf'
+        p.merge_pdfs(page_range, output_file_name)
 
-    p.merge_pdfs(page_range, output_file_name)
+        # Write metadata
+        output_file_path = path.join(args.output_folder, output_file_name)
+        m.write_metadata(doi_metadata, output_file_path)
 
-    # Write metadata
-    output_file_path = path.join(args.output_folder, output_file_name)
-    m.write_metadata(doi_metadata, output_file_path)
+if __name__ == '__main__':
+    run()
