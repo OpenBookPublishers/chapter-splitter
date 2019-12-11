9ebd57ad43d687ab8141519589f1294ffc8b5e04
Javier Arias <javier@arias.re>
2019-12-11 14:47:56 +0000

    enforce pep

--- src/modules/checks.py
+++ src/modules/checks.py
@@ -3,23 +3,25 @@
 from os import path
 import subprocess
 
+
 def file_checks(file):
-    assert path.exists(file), \
-           'the path {} doesn\'t exist'.format(file)
-    assert path.isfile(file), \
-           'the path {} is not a file'.format(file)
+    if not path.exists(file):
+        raise AssertionError('the path {} doesn\'t exist'.format(file))
+    if not path.isfile(file):
+        raise AssertionError('the path {} is not a file'.format(file))
+
 
 def path_checks(folder):
-    assert path.exists(folder), \
-           'the path {} doesn\'t exist'.format(folder)
-    assert path.isdir(folder), \
-           'the path {} is not a folder'.format(folder)
+    if not path.exists(folder):
+        raise AssertionError('the path {} doesn\'t exist'.format(folder))
+    if not path.isdir(folder):
+        raise AssertionError('the path {} is not a folder'.format(folder))
+
 
 def dependencies_checks():
     dependencies = ['pdftk', 'exiftool']
 
     for tool in dependencies:
-        test = subprocess.call(['which', tool])
-
-        assert test == 0, \
-               'Tool {} not installed on your system'.format(tool)
+        if subprocess.call(['which', tool]) != 0:
+            raise AssertionError('Tool {} not installed on your system'
+                                 .format(tool))
