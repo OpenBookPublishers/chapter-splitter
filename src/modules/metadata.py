f5b4bd99fadfff9e57017d4cfee4b003a650e832
Javier Arias <javier@arias.re>
2019-12-11 14:20:48 +0000

    enforce pep

--- src/modules/metadata.py
+++ src/modules/metadata.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import json
-import requests
 from os import path
-from config import Config
 from datetime import datetime
 from subprocess import run
+import requests
+from config import Config
+
 
 class Metadata:
     def __init__(self):
@@ -13,7 +14,9 @@ class Metadata:
         self.api_url = config.get_config('metadata', 'api_url')
 
     def gather_metadata(self, doi):
-        ## Returns a dictionary filled with metadata of doi
+        """
+        Returns a dictionary filled with metadata of doi
+        """
 
         json_data = self.get_json_data(doi)
 
@@ -26,19 +29,23 @@ class Metadata:
                     'author_name_1': self.get_author_name(json_data, 1),
                     'author_name_2': self.get_author_name(json_data, 2),
                     'ISBN': json_data['message']['ISBN'][2]
-        }
+                    }
 
         print('{}: Metadata gathered'.format(doi))
         return metadata
 
     def get_json_data(self, doi):
-        ## Return json data of doi
+        """
+        Return json data of doi
+        """
 
         request = requests.get(''.join([self.api_url, doi]))
         return json.loads(request.text)
 
     def get_author_name(self, json_data, position):
-        ## Returns author name (if any)
+        """
+        Returns author name (if any)
+        """
 
         name = ''
         if len(json_data['message']['author']) > position:
@@ -48,17 +55,20 @@ class Metadata:
         return name
 
     def write_metadata(self, metadata, output_file_path):
-        ## Writes metadata to file_name
+        """
+        Writes metadata to file_name
+
+        TODO add following arguments:
+            '-CreationDate={}'
+            '-Keywords={ }'
+            '-Subject={ }' (abstract)
+        """
 
         arguments = ['-Title={chapter_title}'.format(**metadata),
                      '-Author={}'.format(self.join_author_names(metadata)),
                      '-Producer={publisher_name}'.format(**metadata),
-                     #~ '-CreationDate={}', ## TODO
-                     '-ModDate={}'.format(datetime.now()\
-                                               .strftime("%Y:%m:%d %T")),
-                     #~ '-Keywords={ }', ## TODO
-                     #~ '-Subject={ }', ## TODO (abstract)
-        ]
+                     '-ModDate={}'.format(datetime.now()
+                                          .strftime("%Y:%m:%d %T"))]
 
         cmd = ['exiftool']
         cmd.append('-q')
@@ -66,18 +76,20 @@ class Metadata:
         cmd.append(output_file_path)
 
         run(cmd)
-        print('{}: Metadata written' \
+        print('{}: Metadata written'
               .format(path.split(output_file_path)[1]))
 
     def join_author_names(self, metadata):
-        ## Returns a string with author names, separated by semicolon
+        """
+        Returns a string with author names, separated by semicolon
+        """
 
         metadata_fields = ['author_name_0',
                            'author_name_1',
                            'author_name_2']
 
-        author_names = [metadata[field] \
-                        for field in metadata_fields \
+        author_names = [metadata[field]
+                        for field in metadata_fields
                         if metadata[field]]
 
         return '; '.join(author_names)
