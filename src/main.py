868c723e4b4d0c5815f8702b6d63772ee29f44e7
Javier Arias <javier@arias.re>
2019-12-12 11:51:58 +0000

    allow zip output

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-from os import path
+from os import path, listdir
+import tempfile
+from zipfile import ZipFile
 from modules.doi import Doi
 from modules.pdf import Pdf
 from modules.metadata import Metadata
@@ -23,6 +25,10 @@ def do_split(m, p, output_dir, doi):
     Metadata.write_metadata(doi_metadata, output_file_path)
 
 
+def get_tmp_dir():
+    return tempfile.mkdtemp()
+
+
 def run():
     parser = argparse.ArgumentParser(description='chapter-splitter')
 
@@ -33,12 +39,18 @@ def run():
     parser.add_argument('-d', '--doi',
                         help='The DOI (at book-level) you wish to parse',
                         required=True)
+    parser.add_argument('-c', '--compress-output', dest='compress',
+                        action='store_true',
+                        help='If set it will output a single zip file')
 
     args = parser.parse_args()
 
+    out_dir = args.output_folder
+    tmp_dir = out_dir if not args.compress else get_tmp_dir()
+
     # Check parsed arguments
     file_checks(args.input_file)
-    path_checks(args.output_folder)
+    path_checks(out_dir)
 
     # Check dependencies
     dependencies_checks()
@@ -48,10 +60,18 @@ def run():
     ch_dois = d.discover_ch_dois()
 
     m = Metadata()
-    p = Pdf(args.input_file, args.output_folder)
+    p = Pdf(args.input_file, tmp_dir)
 
     for doi in ch_dois:
-        do_split(m, p, args.output_folder, doi)
+        do_split(m, p, tmp_dir, doi)
+
+    if args.compress:
+        out_file = '{}/{}.zip'.format(out_dir, d.book_level_doi_suffix)
+        suffix = '_original'
+        files = filter(lambda w: not w.endswith(suffix), listdir(tmp_dir))
+        with ZipFile(out_file, 'w') as zipfile:
+            for file in files:
+                zipfile.write('{}/{}'.format(tmp_dir, file), file)
 
 
 if __name__ == '__main__':
