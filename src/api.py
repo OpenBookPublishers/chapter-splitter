2e06db7d8e34693c9a1525d57f34be7c427973a5
Luca Baffa <luca@openbookpublishers.com>
2022-05-05 17:07:06 +0100

    add chapter data class

--- src/api.py
+++ src/api.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
+from dataclasses import asdict
 
 
 @dataclass
 class Book:
     isbn: str = None
     doi: str = None
+    chapters: list = None
 
     @classmethod
     def from_dict(cls, d):
         return Book(**d)
+
+
+@dataclass
+class Chapter:
+    author: str = None
+    title: str = None
+    abstract: str = None
+    pages: list[int] = None
+    doi: str = None
+    licence: str = None
+    container_title: str = None
+    publisher: str = None
+
+    @classmethod
+    def from_dict(cls, d):
+        return Chapter(**d)
+
+    @classmethod
+    def to_dict(self):
+        return asdict(self)
