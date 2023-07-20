1ddbe51b4f3bc461a4d6df348348a0bf0c31c24c
rhigman <73792779+rhigman@users.noreply.github.com>
2023-07-20 12:29:03 +0100

    Write Landing Page and Full Text URLs to Thoth automatically on chapter creation

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from pdf import Pdf
 from metadata import Metadata
 from shutil import copy2
+from thothlibrary import ThothError
 import re
 
 app = typer.Typer()
@@ -18,7 +19,8 @@ def run(input_file:    Path = typer.Option("./file.pdf",
         output_folder: Path = typer.Option("./output/",
                                            exists=True, resolve_path=True),
         doi:            str = typer.Argument(...),
-        database:       str = "thoth"):
+        database:       str = "thoth",
+        write_urls:     bool = True):
 
     with tempfile.TemporaryDirectory() as tmp_dir:
 
@@ -37,12 +39,20 @@ def run(input_file:    Path = typer.Option("./file.pdf",
             # Merge PDFs
             pdf.merge_pdfs(page_range, output_file_name)
 
-            # Write metadata
+            # Write metadata to chapter PDF
             output_file_path = os.path.join(tmp_dir, output_file_name)
             metadata.write_metadata(chapter, output_file_path)
 
             # copy file to output dir
             copy2(output_file_path, output_folder)
+
+            if write_urls:
+                # Write chapter URL metadata to database
+                try:
+                    metadata.write_urls(chapter)
+                except (KeyError, ThothError) as e:
+                    # Continue on error, but display warning
+                    print('Error writing URLs to {}: {}'.format(database, e))
 
 
 if __name__ == '__main__':
